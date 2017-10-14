ee283c77308c0362e7b910564a0e56ceb3944b14
marcmolgar <marcmolgar95@gmail.com>
2017-10-14 15:03:28 +0200

    Botons de login amb Google/Twitter/Facebook implementats

--- AgorApp/app/src/main/java/pes/agorapp/MainActivity.java
+++ AgorApp/app/src/main/java/pes/agorapp/MainActivity.java
@@ -1,8 +1,12 @@
 package pes.agorapp;
 
+import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,13 +16,62 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.FacebookCallback;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.auth.api.Auth;
+
+import com.twitter.sdk.android.core.Twitter;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterAuthToken;
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterAuthClient;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
+
+    private String email;
+    private String loginWith;
+    private ConnectionResult mConnectionResult;
+    private CallbackManager callbackManagerFacebook;
+    private static String TAG = "MainActivity";
+    private static final int REQUEST_CODE_RESOLVE_ERR = 9000;
+    private static final int RC_SIGN_IN_GOOGLE = 1988;
+    private static final int RC_SIGN_IN_TWITTER = TwitterAuthConfig.DEFAULT_AUTH_REQUEST_CODE;
+    private static GoogleApiClient mGoogleApiClient;
+    private TwitterLoginButton loginButtonTwitter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Twitter.initialize(this);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -40,6 +93,89 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        initGoogleComponents();
+        initTwitterComponents();
+        //facebook
+        callbackManagerFacebook = CallbackManager.Factory.create();
+        LoginManager.getInstance().registerCallback(callbackManagerFacebook,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        GraphRequest request = GraphRequest.newMeRequest(AccessToken.getCurrentAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(JSONObject object, GraphResponse response) {
+                                final JSONObject json = response.getJSONObject();
+
+                                try {
+                                    if(json != null){
+                                        email = json.getString("email");
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+                        Bundle parameters = new Bundle();
+                        parameters.putString("fields", "id,name,link,email,picture");
+                        request.setParameters(parameters);
+                        request.executeAsync();
+                        Toast.makeText(getApplicationContext(), "Loguejat amb FACEBOOK\nmail: "+email, Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        Toast.makeText(getApplicationContext(), "Login cancelat! =(", Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        Toast.makeText(getApplicationContext(), "FAIL! =(", Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
+
+    private void initTwitterComponents() {
+        loginButtonTwitter = (TwitterLoginButton) findViewById(R.id.sign_in_button_twitter);
+        loginButtonTwitter.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                TwitterSession session = TwitterCore.getInstance().getSessionManager().getActiveSession();
+                TwitterAuthToken authToken = session.getAuthToken();
+                String token = authToken.token;
+                String secret = authToken.secret;
+                final String[] tmail = new String[1];
+                TwitterAuthClient authClient = new TwitterAuthClient();
+                authClient.requestEmail(session, new Callback<String>() {
+                    @Override
+                    public void success(Result<String> result) {
+                        tmail[0] = String.valueOf(result);
+                    }
+
+                    @Override
+                    public void failure(TwitterException exception) {
+                        tmail[0] = "fail twitter";
+                    }
+                });
+                Toast.makeText(getApplicationContext(), "Loguejat amb TWITTER\nmail: "+tmail[0], Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                Toast.makeText(getApplicationContext(), "FAIL! =(", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void initGoogleComponents() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+        findViewById(R.id.sign_in_button_google).setOnClickListener(this);
     }
 
     @Override
@@ -97,5 +233,69 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        if (mConnectionResult == null) {
+            if (connectionResult.hasResolution()) {
+                try {
+                    connectionResult.startResolutionForResult(this, REQUEST_CODE_RESOLVE_ERR);
+                } catch (IntentSender.SendIntentException e) {
+                    mGoogleApiClient.connect();
+                }
+            }
+        }
+        mConnectionResult = connectionResult;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sign_in_button_google:
+                signInGoogle();
+                break;
+            // ... (altres mètodes de login)
+        }
+    }
+
+    private void signInGoogle() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN_GOOGLE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+            switch (requestCode) {
+                case RC_SIGN_IN_GOOGLE:
+                    GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+                    handleSignInResult(result);
+                    break;
+                case RC_SIGN_IN_TWITTER:
+                    loginButtonTwitter.onActivityResult(requestCode, resultCode, data);
+                    break;
+                default:
+                    callbackManagerFacebook.onActivityResult(requestCode, resultCode, data);
+                    break;
+            }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            String gmail = acct.getEmail();
+            String gname = acct.getDisplayName();
+            String gphoto = String.valueOf(acct.getPhotoUrl());
+            Toast.makeText(getApplicationContext(), "Loguejat amb GOOGLE:\nmail: "+
+                    gmail+"\nnom: "+gname+"\nphotourl: "+gphoto, Toast.LENGTH_LONG).show();
+        } else {
+            // Signed out, show unauthenticated UI.
+            TextView myAwesomeTextView = (TextView)findViewById(R.id.text_prova);
+            Toast.makeText(getApplicationContext(), "FAIL! =(", Toast.LENGTH_LONG).show();
+        }
     }
 }
