c0d710ee1a3717f282374351a6505ec76459693b
Daniel Garrapucho <dangarrapucho@gmail.com>
2017-10-16 23:23:54 +0200

    Implementar  operacions CREATE i DESTROY del controlador d'usuaris. Comentar futurs tests de integracio del controlador

--- Backend/AgorApp/app/controllers/users_controller.rb
+++ Backend/AgorApp/app/controllers/users_controller.rb
@@ -1,12 +1,23 @@
 class UsersController < ApplicationController
-  
+
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def index
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    respond_to do |format|
+      if @user.save
+        format.json { render json: @user.as_json(root: false, only: [:active_token]) , status: :created }
+      else
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def show
@@ -19,13 +30,31 @@ class UsersController < ApplicationController
   end
 
   def destroy
+    @user.destroy
+    respond_to do |format|
+      format.json { head :no_content }
+    end
   end
-  
+
   private
 ################################################################################
-  
+
   def user_params
-    params.permit(:name, :image_url, :platform_name, :active_token)
+    params.permit(:name, :image_url, :platform_name)
   end
-  
+
+  #TODO escollir un dels metodes per la instancia d'usuari actual
+
+  #OPCIO-1: active_token
+  def set_user
+      @user = User.find(params[:active_token])
+  end
+
+  #OPCIO-2: id
+
+  #def set_user
+  #    @user = User.find(params[:id])
+  #end
+
+
 end
--- Backend/AgorApp/test/controllers/users_controller_test.rb
+++ Backend/AgorApp/test/controllers/users_controller_test.rb
@@ -1,10 +1,15 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+
+  #TODO test-1: INDEX responses shouldn't include active_token in the JSON
+  #TODO test-2: CREATE responses shall include active_token in the JSON
+  
   test "should get index" do
     get users_index_url
     assert_response :success
   end
+
 
   test "should get new" do
     get users_new_url
