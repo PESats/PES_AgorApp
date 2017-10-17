d6767c1334429f6e647db5cebc0197cc51349db9
Marti Lloveras <marti-nini@hotmail.com>
2017-10-17 13:31:56 +0200

    Add restful user routes

--- Backend/AgorApp/config/routes.rb
+++ Backend/AgorApp/config/routes.rb
@@ -1,17 +1,20 @@
 Rails.application.routes.draw do
-  get 'users/index'
+  
+ resources :users
+ 
+ #get 'users/index'
 
-  get 'users/new'
+ #get 'users/new'
 
-  get 'users/create'
+ #get 'users/create'
 
-  get 'users/show'
+ #get 'users/show'
 
-  get 'users/edit'
+ #get 'users/edit'
 
-  get 'users/update'
+ #get 'users/update'
 
-  get 'users/destroy'
+ #get 'users/destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
