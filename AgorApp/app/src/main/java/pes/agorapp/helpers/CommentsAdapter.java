a7975f8046cf080331d4d353310ba2ce8c7bc887
Marc Molinuevo <marcmolgar95@gmail.com>
2017-11-12 16:28:36 +0100

    preparacio crides API a editar/esborrar comentari

--- AgorApp/app/src/main/java/pes/agorapp/helpers/CommentsAdapter.java
+++ AgorApp/app/src/main/java/pes/agorapp/helpers/CommentsAdapter.java
@@ -12,10 +12,23 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.gson.JsonObject;
+
+import okhttp3.Request;
+import pes.agorapp.JSONObjects.UserAgorApp;
+import pes.agorapp.activities.LoginActivity;
+import pes.agorapp.customComponents.DialogServerKO;
+import pes.agorapp.globals.PreferencesAgorApp;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import java.io.IOException;
 import java.util.List;
 
 import pes.agorapp.JSONObjects.Comment;
 import pes.agorapp.R;
+import pes.agorapp.network.AgorAppApiManager;
 
 /**
  * Created by Alex on 01-Nov-17.
@@ -50,6 +63,36 @@ public class CommentsAdapter extends ArrayAdapter<Comment> {
             @Override
             public void onClick(View v) {
                 Toast.makeText(CommentsAdapter.super.getContext(), "Editar: " + comment.getId(), Toast.LENGTH_LONG).show();
+                /*
+                int commentId = comment.getId();
+
+                PreferencesAgorApp prefs = new PreferencesAgorApp(CommentsAdapter.super.getContext());
+
+                String userId = prefs.getId();
+                String token = prefs.getActiveToken();
+
+                JsonObject jsonUser = new JsonObject();
+                jsonUser.addProperty("id", userId);
+                jsonUser.addProperty("active_token", token);
+
+                JsonObject json = new JsonObject();
+                json.add("user", jsonUser);
+
+                AgorAppApiManager
+                        .getService()
+                        .editComment(commentId, json)
+                        .enqueue(new retrofit2.Callback<Comment>() {
+                            @Override
+                            public void onResponse(Call<Comment> call, Response<Comment> response) {
+                                //
+                            }
+
+                            @Override
+                            public void onFailure(Call<Comment> call, Throwable t) {
+                                //
+                            }
+                        });
+                */
             }
         });
         Button buttonDelete = (Button) convertView.findViewById(R.id.comment_delete);
@@ -57,6 +100,36 @@ public class CommentsAdapter extends ArrayAdapter<Comment> {
             @Override
             public void onClick(View v) {
                 Toast.makeText(CommentsAdapter.super.getContext(), "Esborrar: " + comment.getId(), Toast.LENGTH_LONG).show();
+                /*
+                int commentId = comment.getId();
+
+                PreferencesAgorApp prefs = new PreferencesAgorApp(CommentsAdapter.super.getContext());
+
+                String userId = prefs.getId();
+                String token = prefs.getActiveToken();
+
+                JsonObject jsonUser = new JsonObject();
+                jsonUser.addProperty("id", userId);
+                jsonUser.addProperty("active_token", token);
+
+                JsonObject json = new JsonObject();
+                json.add("user", jsonUser);
+
+                AgorAppApiManager
+                        .getService()
+                        .deleteComment(commentId, json)
+                        .enqueue(new retrofit2.Callback<Comment>() {
+                            @Override
+                            public void onResponse(Call<Comment> call, Response<Comment> response) {
+                                //
+                            }
+
+                            @Override
+                            public void onFailure(Call<Comment> call, Throwable t) {
+                                //
+                            }
+                        });
+                */
             }
         });
         if (comment.getId() != 1) {
